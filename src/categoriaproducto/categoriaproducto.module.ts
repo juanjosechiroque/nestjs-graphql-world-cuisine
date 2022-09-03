8490d3db17ade321c84a5e8ea3502671292a4935
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-02 23:34:06 -0500

    Ajuste en modulos para arreglar error de ejecucion

--- src/categoriaproducto/categoriaproducto.module.ts
+++ src/categoriaproducto/categoriaproducto.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriaproductoService } from './categoriaproducto.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaproductoEntity } from './categoriaproducto.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoriaproductoEntity])],
   providers: [CategoriaproductoService]
 })
 export class CategoriaproductoModule {}
