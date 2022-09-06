19da7c9a6fb8ca8f522b572421e01e7f1b53dfdf
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-05 23:16:05 -0500

    Desarrollo controlador categoriaproducto

--- src/categoriaproducto/categoriaproducto.controller.ts
+++ src/categoriaproducto/categoriaproducto.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { CategoriaproductoDto } from './categoriaproducto.dto';
+import { CategoriaproductoEntity } from './categoriaproducto.entity';
+import { CategoriaproductoService } from './categoriaproducto.service';
 
-@Controller('categoriaproducto')
-export class CategoriaproductoController {}
+@Controller('categoriasproducto')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class CategoriaproductoController {
+    constructor(private readonly categoriaproductoService: CategoriaproductoService) {}
+
+  @Get()
+  async findAll() {
+    return await this.categoriaproductoService.findAll();
+  }
+
+  @Get(':categoriaproductoCodigo')
+  async findOne(@Param('categoriaproductoCodigo') categoriaproductoCodigo: string) {
+    return await this.categoriaproductoService.findOne(categoriaproductoCodigo);
+  }
+
+  @Post()
+  async create(@Body() categoriaproductoDto: CategoriaproductoDto) {
+    const categoriaproducto: CategoriaproductoEntity = plainToInstance(CategoriaproductoEntity, categoriaproductoDto);
+    return await this.categoriaproductoService.create(categoriaproducto);
+  }
+
+  @Put(':categoriaproductoCodigo')
+  async update(@Param('categoriaproductoCodigo') categoriaproductoCodigo: string, @Body() categoriaproductoDto: CategoriaproductoDto) {
+    const categoriaproducto: CategoriaproductoEntity = plainToInstance(CategoriaproductoEntity, categoriaproductoDto);
+    return await this.categoriaproductoService.update(categoriaproductoCodigo, categoriaproducto);
+  }
+
+  @Delete(':categoriaproductoCodigo')
+  @HttpCode(204)
+  async delete(@Param('categoriaproductoCodigo') categoriaproductoCodigo: string) {
+    return await this.categoriaproductoService.delete(categoriaproductoCodigo);
+  }
+
+}
