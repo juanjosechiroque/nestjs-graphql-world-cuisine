aaaed390edc73e4923f9cf3d946e7b1526a54e2f
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-05 22:56:52 -0500

    Interceptor de errores y creacion de controlador y DTO para recurso categoriaproducto

---
+++ src/categoriaproducto/categoriaproducto.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('categoriaproducto')
+export class CategoriaproductoController {}
--- src/categoriaproducto/categoriaproducto.module.ts
+++ src/categoriaproducto/categoriaproducto.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoriaproductoService } from './categoriaproducto.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaproductoEntity } from './categoriaproducto.entity';
+import { CategoriaproductoController } from './categoriaproducto.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CategoriaproductoEntity])],
-  providers: [CategoriaproductoService]
+  providers: [CategoriaproductoService],
+  controllers: [CategoriaproductoController]
 })
 export class CategoriaproductoModule {}
