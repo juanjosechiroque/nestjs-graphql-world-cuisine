77280c1ed664df2c968c89edc35cb792eefb942a
rmatiz84 <r.matiz@uniandes.edu.co>
2022-09-05 22:22:55 -0500

    arreglo modulo

--- src/restaurante-ciudad/restaurante-ciudad.module.ts
+++ src/restaurante-ciudad/restaurante-ciudad.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RestauranteCiudadService } from './restaurante-ciudad.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestauranteEntity } from '../restaurante/restaurante.entity';
+import { CiudadEntity } from '../ciudad/ciudad.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RestauranteEntity, CiudadEntity])],
   providers: [RestauranteCiudadService]
 })
 export class RestauranteCiudadModule {}
