22cb0acfa96d3c41798e58193d5bbd0905b38ca4
Hector Varon Bonilla <h.varon@uniandes.edu.co>
2022-09-08 13:13:04 -0500

    Desarrollo controlador recurso país
    Desarrollo controlador País

--- src/pais/pais.module.ts
+++ src/pais/pais.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PaisService } from './pais.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaisEntity } from './pais.entity';
+import { PaisController } from './pais.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PaisEntity])],
-  providers: [PaisService]
+  providers: [PaisService],
+  controllers: [PaisController]
 })
 export class PaisModule {}
