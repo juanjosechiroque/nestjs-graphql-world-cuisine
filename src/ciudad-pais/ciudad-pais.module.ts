dd27762492e6ff9e9d3316612a2d69da7e6893f7
Hector Varon Bonilla <h.varon@uniandes.edu.co>
2022-09-08 13:53:46 -0500

    Desarrollo controlador recurso asociación ciudad-pais

--- src/ciudad-pais/ciudad-pais.module.ts
+++ src/ciudad-pais/ciudad-pais.module.ts
@@ -3,9 +3,11 @@ import { CiudadPaisService } from './ciudad-pais.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CiudadEntity } from '../ciudad/ciudad.entity';
 import { PaisEntity } from '../pais/pais.entity';
+import { CiudadPaisController } from './ciudad-pais.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CiudadEntity, PaisEntity])],
-    providers: [CiudadPaisService]
+    providers: [CiudadPaisService],
+    controllers: [CiudadPaisController]
 })
 export class CiudadPaisModule {}
