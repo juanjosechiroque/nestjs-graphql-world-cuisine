f0167c00890aa8a572092ae64894d291d66c9246
rmatiz84 <r.matiz@uniandes.edu.co>
2022-09-17 00:17:19 -0500

    se agrega seguridad en controladores

--- src/cultura-receta/cultura-receta.controller.ts
+++ src/cultura-receta/cultura-receta.controller.ts
@@ -1,32 +1,41 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { HasRoles } from '../user/roles.decorator';
+import { Role } from '../user/role.enum';
 import { RecetaEntity } from 'src/receta/receta.entity';
 import { RecetaDto } from '../receta/receta.dto';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { CulturaRecetaService } from './cultura-receta.service';
 
 @Controller('culturas')
-@UseInterceptors(BusinessErrorsInterceptor)
+@UseGuards(JwtAuthGuard, RolesGuard)
+@UseInterceptors(BusinessErrorsInterceptor) 
 export class CulturaRecetaController {
 
     constructor(private readonly culturaRecetaService: CulturaRecetaService){}
 
     @Post(':culturaCodigo/recetas/:recetaCodigo')
+    @HasRoles(Role.ADMIN)
     async addRecetaCultura(@Param('culturaCodigo') culturaCodigo: string, @Param('recetaCodigo') recetaCodigo: string){
         return await this.culturaRecetaService.addRecetaCultura(culturaCodigo, recetaCodigo);
     }
 
     @Get(':culturaCodigo/recetas/:recetaCodigo')
+    @HasRoles(Role.ADMIN,Role.READER)
     async findRecetaByCulturaIdRecetaId(@Param('culturaCodigo') culturaCodigo: string, @Param('recetaCodigo') recetaCodigo: string){
         return await this.culturaRecetaService.findRecetaByCulturaIdRecetaId(culturaCodigo, recetaCodigo);
     }
 
     @Get(':culturaCodigo/recetas')
+    @HasRoles(Role.ADMIN,Role.READER)
     async findRecetasByCulturaId(@Param('culturaCodigo') culturaCodigo: string){
         return await this.culturaRecetaService.findRecetasByCulturaId(culturaCodigo);
     }
 
     @Put(':culturaCodigo/recetas')
+    @HasRoles(Role.ADMIN)
     async associateRecetasCultura(@Body() recetasDto: RecetaDto[], @Param('culturaCodigo') culturaCodigo: string){
         const recetas = plainToInstance(RecetaEntity, recetasDto)
         return await this.culturaRecetaService.associateRecetasCultura(culturaCodigo, recetas);
@@ -34,6 +43,7 @@ export class CulturaRecetaController {
     
     @Delete(':culturaCodigo/recetas/:recetaCodigo')
     @HttpCode(204)
+    @HasRoles(Role.ADMIN)
     async deleteRecetaCultura(@Param('culturaCodigo') culturaCodigo: string, @Param('recetaCodigo') recetaCodigo: string){
         return await this.culturaRecetaService.deleteRecetaCultura(culturaCodigo, recetaCodigo);
     }
--- src/cultura/cultura.controller.ts
+++ src/cultura/cultura.controller.ts
@@ -1,32 +1,41 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { HasRoles } from '../user/roles.decorator';
+import { Role } from '../user/role.enum';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { CulturaDto } from './cultura.dto';
 import { CulturaEntity } from './cultura.entity';
 import { CulturaService } from './cultura.service';
 
-@Controller('culturas')
+@Controller('culturas') 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @UseInterceptors(BusinessErrorsInterceptor)
 export class CulturaController {
     constructor(private readonly culturaService: CulturaService) {}
 
   @Get()
+  @HasRoles(Role.ADMIN,Role.READER)
   async findAll() {
     return await this.culturaService.findAll();
   }
 
   @Get(':culturaCodigo')
+  @HasRoles(Role.ADMIN,Role.READER)
   async findOne(@Param('culturaCodigo') culturaCodigo: string) {
     return await this.culturaService.findOne(culturaCodigo);
   }
 
   @Post()
+  @HasRoles(Role.ADMIN)
   async create(@Body() culturaDto: CulturaDto) {
     const ciudad: CulturaEntity = plainToInstance(CulturaEntity, culturaDto);
     return await this.culturaService.create(ciudad);
   }
 
   @Put(':culturaCodigo')
+  @HasRoles(Role.ADMIN)
   async update(@Param('culturaCodigo') culturaCodigo: string, @Body() culturaDto: CulturaDto) {
     const cultura: CulturaEntity = plainToInstance(CulturaEntity, culturaDto);
     return await this.culturaService.update(culturaCodigo, cultura);
@@ -34,6 +43,7 @@ export class CulturaController {
 
   @Delete(':culturaCodigo')
   @HttpCode(204)
+  @HasRoles(Role.ADMIN)
   async delete(@Param('culturaCodigo') culturaCodigo: string) {
     return await this.culturaService.delete(culturaCodigo);
   }
