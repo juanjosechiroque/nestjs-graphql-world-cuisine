bfa8e509a58aae5fe966781e806614615b1061bc
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-21 00:06:12 -0500

    Incorporación de caché en recurso categoriaproducto

--- src/categoriaproducto/categoriaproducto.module.ts
+++ src/categoriaproducto/categoriaproducto.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { CategoriaproductoService } from './categoriaproducto.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaproductoEntity } from './categoriaproducto.entity';
 import { CategoriaproductoController } from './categoriaproducto.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CategoriaproductoEntity])],
+  imports: [TypeOrmModule.forFeature([CategoriaproductoEntity]), CacheModule.register({ttl: 30})],
   providers: [CategoriaproductoService],
   controllers: [CategoriaproductoController]
 })
--- src/categoriaproducto/categoriaproducto.service.ts
+++ src/categoriaproducto/categoriaproducto.service.ts
@@ -1,19 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoriaproductoEntity } from './categoriaproducto.entity';
 import { Repository } from 'typeorm';
 import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class CategoriaproductoService {
 
+    cacheKey: string = "categoriasproducto";
+
     constructor(
         @InjectRepository(CategoriaproductoEntity)
-        private readonly categoriaproductoRepository: Repository<CategoriaproductoEntity>
+        private readonly categoriaproductoRepository: Repository<CategoriaproductoEntity>,
+        @Inject(CACHE_MANAGER)
+        private readonly cacheManager: Cache        
     ){}
 
     async findAll(): Promise<CategoriaproductoEntity[]> {
-        return await this.categoriaproductoRepository.find();
+
+        const cached: CategoriaproductoEntity[] = await this.cacheManager.get<CategoriaproductoEntity[]>(this.cacheKey);
+
+        if(!cached) {
+            const categoriasproducto: CategoriaproductoEntity[] = await this.categoriaproductoRepository.find();
+            await this.cacheManager.set(this.cacheKey, categoriasproducto);
+            return categoriasproducto;
+        }
+        return cached;
+
     }
 
     async findOne(codigo: string): Promise<CategoriaproductoEntity> {
