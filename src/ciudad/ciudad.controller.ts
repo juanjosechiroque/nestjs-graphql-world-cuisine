697f85a2b6c69e0ac735ca6f9fd1e2ecfdd862cf
Hector Varon Bonilla <h.varon@uniandes.edu.co>
2022-09-22 11:04:13 -0500

    Implementacion de seguridad controladores pais y ciudad

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -1,33 +1,42 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { HasRoles } from '../user/roles.decorator';
+import { Role } from '../user/role.enum';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { CiudadDto } from './ciudad.dto';
 import { CiudadEntity } from './ciudad.entity';
 import { CiudadService } from './ciudad.service';
 
 @Controller('ciudades')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @UseInterceptors(BusinessErrorsInterceptor)
 export class CiudadController {
     constructor(private readonly ciudadService: CiudadService) {}
 
   @Get()
+  @HasRoles(Role.ADMIN,Role.READER)
   async findAll() {
     return await this.ciudadService.findAll();
   }
 
   @Get(':ciudadCodigo')
+  @HasRoles(Role.ADMIN,Role.READER)
   async findOne(@Param('ciudadCodigo') ciudadCodigo: string) {
     return await this.ciudadService.findOne(ciudadCodigo);
   }
 
   @Post()
+  @HasRoles(Role.ADMIN)
   async create(@Body() ciudadDto: CiudadDto) {
     const ciudad: CiudadEntity = plainToInstance(CiudadEntity, ciudadDto);
     return await this.ciudadService.create(ciudad);
   }
 
   @Put(':ciudadCodigo')
+  @HasRoles(Role.ADMIN)
   async update(@Param('ciudadCodigo') ciudadCodigo: string, @Body() ciudadDto: CiudadDto) {
     const ciudad: CiudadEntity = plainToInstance(CiudadEntity, ciudadDto);
     return await this.ciudadService.update(ciudadCodigo, ciudad);
@@ -35,6 +44,7 @@ export class CiudadController {
 
   @Delete(':ciudadCodigo')
   @HttpCode(204)
+  @HasRoles(Role.ADMIN)
   async delete(@Param('ciudadCodigo') ciudadCodigo: string) {
     return await this.ciudadService.delete(ciudadCodigo);
   }
--- src/pais/pais.controller.ts
+++ src/pais/pais.controller.ts
@@ -1,33 +1,42 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { HasRoles } from '../user/roles.decorator';
+import { Role } from '../user/role.enum';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { PaisDto } from './pais.dto';
 import { PaisEntity } from './pais.entity';
 import { PaisService } from './pais.service';
 
 @Controller('paises')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @UseInterceptors(BusinessErrorsInterceptor)
 export class PaisController {
     constructor(private readonly paisService: PaisService) {}
 
   @Get()
+  @HasRoles(Role.ADMIN,Role.READER)
   async findAll() {
     return await this.paisService.findAll();
   }
 
   @Get(':paisCodigo')
+  @HasRoles(Role.ADMIN,Role.READER)
   async findOne(@Param('paisCodigo') paisCodigo: string) {
     return await this.paisService.findOne(paisCodigo);
   }
 
   @Post()
+  @HasRoles(Role.ADMIN)
   async create(@Body() paisDto: PaisDto) {
     const pais: PaisEntity = plainToInstance(PaisEntity, paisDto);
     return await this.paisService.create(pais);
   }
 
   @Put(':paisCodigo')
+  @HasRoles(Role.ADMIN)
   async update(@Param('paisCodigo') paisCodigo: string, @Body() paisDto: PaisDto) {
     const pais: PaisEntity = plainToInstance(PaisEntity, paisDto);
     return await this.paisService.update(paisCodigo, pais);
@@ -35,6 +44,7 @@ export class PaisController {
 
   @Delete(':paisCodigo')
   @HttpCode(204)
+  @HasRoles(Role.ADMIN)
   async delete(@Param('paisCodigo') paisCodigo: string) {
     return await this.paisService.delete(paisCodigo);
   }
