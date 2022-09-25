ff47c25c9bd8c6065a4389c6711ed72b66f30391
rmatiz84 <r.matiz@uniandes.edu.co>
2022-09-24 21:06:17 -0500

    se actualiza modulo con cache

--- src/cultura-receta/cultura-receta.module.ts
+++ src/cultura-receta/cultura-receta.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { CulturaRecetaService } from './cultura-receta.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CulturaEntity } from '../cultura/cultura.entity';
 import { RecetaEntity } from '../receta/receta.entity';
 import { CulturaRecetaController } from './cultura-receta.controller';
-
+ 
 @Module({
-  imports: [TypeOrmModule.forFeature([CulturaEntity, RecetaEntity])],
+  imports: [TypeOrmModule.forFeature([CulturaEntity, RecetaEntity]), CacheModule.register({ttl: 30})],
   providers: [CulturaRecetaService],
   controllers: [CulturaRecetaController]
 })
