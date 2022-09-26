4dca5a1dc5358cb127c1eb0295a66cc507dd9b1f
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-26 15:50:52 -0500

    Se incluye esquema de grapgQL para categoria producto y producto.  Se implementa el resolver para categoriaproducto

--- src/categoriaproducto/categoriaproducto.entity.ts
+++ src/categoriaproducto/categoriaproducto.entity.ts
@@ -1,15 +1,20 @@
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 import { ProductoEntity } from "../producto/producto.entity";
+import { Field, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 @Entity()
 export class CategoriaproductoEntity {
 
+    @Field()
     @PrimaryGeneratedColumn("uuid")
     codigo: string;
 
+    @Field()
     @Column()
     nombre: string;
 
+    @Field(type => [ProductoEntity])
     @OneToMany(() => ProductoEntity, producto => producto.categoriaProducto)
     productos: ProductoEntity[];
 
--- src/categoriaproducto/categoriaproducto.module.ts
+++ src/categoriaproducto/categoriaproducto.module.ts
@@ -3,10 +3,11 @@ import { CategoriaproductoService } from './categoriaproducto.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaproductoEntity } from './categoriaproducto.entity';
 import { CategoriaproductoController } from './categoriaproducto.controller';
+import { CategoriaproductoResolver } from './categoriaproducto.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CategoriaproductoEntity]), CacheModule.register({ttl: 30})],
-  providers: [CategoriaproductoService],
+  providers: [CategoriaproductoService, CategoriaproductoResolver],
   controllers: [CategoriaproductoController]
 })
 export class CategoriaproductoModule {}
