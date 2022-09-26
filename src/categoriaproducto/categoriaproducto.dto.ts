40c63b3a0563912dba8e61b138b6b72688a8892e
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-26 16:11:39 -0500

    Desarrollo completo de GraphQL en recurso Categoría de producto

--- src/categoriaproducto/categoriaproducto.dto.ts
+++ src/categoriaproducto/categoriaproducto.dto.ts
@@ -1,7 +1,12 @@
+import { Field, InputType } from '@nestjs/graphql';
 import {IsNotEmpty, IsString} from 'class-validator';
 
+@InputType()
 export class CategoriaproductoDto {
+    @Field()
     readonly codigo: string;
+    
+    @Field()
     @IsString()
     @IsNotEmpty()
     readonly nombre: string;
--- src/categoriaproducto/categoriaproducto.resolver.ts
+++ src/categoriaproducto/categoriaproducto.resolver.ts
@@ -1,6 +1,8 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CategoriaproductoService } from './categoriaproducto.service';
 import { CategoriaproductoEntity } from './categoriaproducto.entity';
+import { plainToInstance } from 'class-transformer';
+import { CategoriaproductoDto } from './categoriaproducto.dto';
 
 @Resolver()
 export class CategoriaproductoResolver {
@@ -16,4 +18,23 @@ export class CategoriaproductoResolver {
         return this.categoriaproductoService.findOne(codigo);
     }
    
+    @Mutation(() => CategoriaproductoEntity)
+    createCategoriaproducto(@Args('categoriaproducto') categoriaproductoDto: CategoriaproductoDto): Promise<CategoriaproductoEntity> {
+        const categoriaproducto = plainToInstance(CategoriaproductoEntity, categoriaproductoDto);
+        return this.categoriaproductoService.create(categoriaproducto);
+    }
+ 
+    @Mutation(() => CategoriaproductoEntity)
+    updateCategoriaproducto(@Args('codigo') codigo: string, @Args('categoriaproducto') categoriaproductoDto: CategoriaproductoDto): Promise<CategoriaproductoEntity> {
+        const categoriaproducto = plainToInstance(CategoriaproductoEntity, categoriaproductoDto);
+        return this.categoriaproductoService.update(codigo, categoriaproducto);
+    }
+ 
+    @Mutation(() => String)
+    deleteCategoriaproducto(@Args('codigo') codigo: string) {
+        this.categoriaproductoService.delete(codigo);
+        return codigo;
+    }
+ 
+
 }
