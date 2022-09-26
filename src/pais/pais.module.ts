e429c2b186da5f167616d6102dad78d73a5340f4
Hector Varon Bonilla <h.varon@uniandes.edu.co>
2022-09-26 14:15:05 -0500

    Incorporacion de cache en recurso pais

--- src/pais/pais.module.ts
+++ src/pais/pais.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { PaisService } from './pais.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaisEntity } from './pais.entity';
 import { PaisController } from './pais.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PaisEntity])],
+  imports: [TypeOrmModule.forFeature([PaisEntity]), CacheModule.register({ttl: 30})],
   providers: [PaisService],
   controllers: [PaisController]
 })
--- src/pais/pais.service.spec.ts
+++ src/pais/pais.service.spec.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { faker } from '@faker-js/faker';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { CacheModule } from '@nestjs/common';
 
 describe('PaisService', () => {
   let service: PaisService;
@@ -13,7 +14,7 @@ describe('PaisService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [...TypeOrmTestingConfig()],
+      imports: [...TypeOrmTestingConfig(), CacheModule.register()],
       providers: [PaisService],
     }).compile();
 
