6e8cfc27b5225a4e47b43da74e88aa6a7b46b237
Hector Varon Bonilla <h.varon@uniandes.edu.co>
2022-09-26 21:48:43 -0500

    Adecuación para mutaciones dto pais, ciudad

--- src/pais/pais.dto.ts
+++ src/pais/pais.dto.ts
@@ -1,6 +1,13 @@
+import { Field, InputType } from '@nestjs/graphql';
 import {IsNotEmpty, IsString} from 'class-validator';
+
+@InputType()
 export class PaisDto {
+    
+    @Field()
     readonly codigo: string;
+    
+    @Field()
     @IsString()
     @IsNotEmpty()
     readonly nombre: string;
