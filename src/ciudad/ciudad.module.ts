28d4c4d7868ab7c02b77188cdab9d14822b003cf
Hector Varon Bonilla <h.varon@uniandes.edu.co>
2022-09-26 21:54:56 -0500

    Desarrollo de GraphQL en recurso ciudad

--- src/ciudad/ciudad.module.ts
+++ src/ciudad/ciudad.module.ts
@@ -3,10 +3,11 @@ import { CiudadService } from './ciudad.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CiudadEntity } from './ciudad.entity';
 import { CiudadController } from './ciudad.controller';
+import { CiudadResolver } from './ciudad.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CiudadEntity]), CacheModule.register({ttl: 30})],
-  providers: [CiudadService],
+  providers: [CiudadService, CiudadResolver],
   controllers: [CiudadController]
 })
 export class CiudadModule {}
