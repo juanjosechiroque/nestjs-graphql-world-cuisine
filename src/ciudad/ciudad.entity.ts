494a8896b771db820fcf87ad4f73423df5858d2d
Hector Varon Bonilla <h.varon@uniandes.edu.co>
2022-09-26 21:41:01 -0500

    Adecuación queries entidad pais, ciudad

--- src/ciudad/ciudad.entity.ts
+++ src/ciudad/ciudad.entity.ts
@@ -1,15 +1,20 @@
 import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 import { PaisEntity } from "../pais/pais.entity";
+import { Field, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 @Entity()
 export class CiudadEntity {
 
+    @Field()
     @PrimaryGeneratedColumn("uuid")
     codigo: string;
     
+    @Field()
     @Column()
     nombre: string;
     
+    @Field(type => PaisEntity)
     @ManyToOne(() => PaisEntity, pais => pais.ciudades)
     pais: PaisEntity;    
 
